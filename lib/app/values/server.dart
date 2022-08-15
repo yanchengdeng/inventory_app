174b40a719feb7c887a6a291bb17cbd8d4a7104f
yanchengdeng <yanchengdeng@gmail.com>
2022-08-15 18:10:15 +0800

    服务环境配置

--- lib/app/values/server.dart
+++ lib/app/values/server.dart
@@ -1,19 +1,20 @@
 enum Environment { DEVELOPMENT, SIT, PRODUCTION }
 
 ///为当前环境
-const SERVER_ENV = Environment.SIT;
+const SERVER_ENV = Environment.DEVELOPMENT;
 
 ///本地测试
 ///接口域名统一配置  token失效返回 585
-/// const SERVER_API_URL = 'http://47.102.199.31:59101/rfidapp/rest';
+const SERVER_API_URL = 'http://47.102.199.31:59101/rfidapp/rest';
+
 ///文件服务域名统一配置
-/// const SERVER_FILE_UPLOAD = 'https://csapi-qa.saic-gm.com/cs';
+const SERVER_FILE_UPLOAD = 'https://csapi-qa.saic-gm.com/cs';
 
 ///SIT 测试使用 统一改用改域名
-const SERVER_API_URL =
+const SERVER_API_URL1 =
     'https://rfid-native-api.apps-qa.saic-gm.com/MidNodeJS/rest';
 
-const SERVER_FILE_UPLOAD =
+const SERVER_FILE_UPLOAD1 =
     'https://rfid-native-api.apps-qa.saic-gm.com/MidNodeJS/rest';
 
 ///文件服务地址  异常返回 500  已废弃

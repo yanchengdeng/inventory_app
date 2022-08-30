9f323424b1c0a5296be9f581f5da157ebee51048
yanchengdeng <yanchengdeng@gmail.com>
2022-08-30 11:20:41 +0800

    经纬度顺序调换

--- lib/app/modules/mould_read_result/views/mould_read_result_view.dart
+++ lib/app/modules/mould_read_result/views/mould_read_result_view.dart
@@ -172,7 +172,7 @@ class MouldReadResultView extends GetView<MouldReadResultController> {
                       (controller.assertBindTaskInfo.value.address?.isEmpty ==
                               true)
                           ? ""
-                          : "${controller.assertBindTaskInfo.value.lat ?? ''},${controller.assertBindTaskInfo.value.lng ?? ''}",
+                          : "${controller.assertBindTaskInfo.value.lng ?? ''},${controller.assertBindTaskInfo.value.lat ?? ''}",
                       style: textSmallTextStyle(),
                     ),
                     Spacer(flex: 1),
